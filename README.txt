e62b0dd6116338ecd03fa337313b13d85d9e808a
Sander <sander-oostenbrink@hotmail.com>
2019-01-15 02:27:08 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,18 @@
+Deze website is gemaakt uit opdracht van de opleiding Communication & multimedia design te Amsterdam.
+
+De website is bedoeld voor mensen die met het rotterdamse openbaar vervoer(RET) reizen en zin hebben om wat te lezen voor onderweg.
+Zo vinden de gebruikers 99 variaties op hetzelfde verhaal en al deze verhalen zijn uniek vorm gegeven.
+De verhalen varieren van zowel genre als van leestijd en zijn alle gemaakt door verschillende studenten van onze klas.
+De website heeft momenteel een verhaal overzicht pagina en een verhaal pagina.
+
+Voor het maken van de website is gebruik gemaakt van:
+HTML5
+CSS3
+Adobe Illustrator CC 2019
+Sublime text 3
+
+15 Januari 2019
+Sander Oosenbrink
+Project Web 
+Blok 2
+Klas 209
---
+++ javascript.js
@@ -0,0 +1,38 @@
+// variablen defineren 
+var search = document.querySelector('#search');
+var kruis = document.querySelector('#terug');
+var filter = document.querySelector('aside button');
+var nav = document.querySelector('header');
+var dropdown = document.querySelector('fieldset');
+var notify = document.querySelector('.profiel');
+var download = document.querySelector('article header button');
+
+// navigatie balk rood laten worden
+search.addEventListener("click", function(){
+	nav.classList.toggle("search");
+	});
+// kruisje indrukken om de navigatiebalk weer wit te maken
+kruis.addEventListener("click", function(){
+	nav.classList.remove("search");
+	});
+// Dropdown class toevoegen om deze uit te klappen
+filter.addEventListener("click", function(){
+	dropdown.classList.toggle("filter");
+	});
+//Download knop groter maken en notificatie geven
+download.addEventListener("click", function(){
+	download.classList.toggle("downloaded");
+	notify.classList.toggle("notify");
+	});
+//Header hoogte uitreken
+  const header_height = nav.offsetHeight
+  window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+    // als de scroll positie even hoog is als de navbar dan veranderd deze naar de witte navbar op groot scherm
+    if (scrollpos >= header_height) { 
+    	nav.classList.add("scrolled");
+    }
+    else { 
+    	nav.classList.remove("scrolled");
+     }
+  })
